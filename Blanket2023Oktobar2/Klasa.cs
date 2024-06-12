7623097cae22a74d2aa84acc052ed3dd17c6e09b
Harascho1 <142120455+Harascho1@users.noreply.github.com>
2024-06-12 19:47:47 +0200

    evo i ovde

---
+++ Blanket2023Oktobar2/Klasa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blanket2023Oktobar2
+{
+    internal class Klasa
+    {
+        public static int SumaKvadrata(params int[] lista)
+        {
+            int suma = 0;
+            foreach (int x in lista)
+            {
+                suma += x * x;
+            }
+            return suma;
+        }
+    }
+}
---
+++ Blanket2023Oktobar2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blanket2023Oktobar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Klasa.SumaKvadrata(1, 2, 3, 4, 5));
+        }
+    }
+}
